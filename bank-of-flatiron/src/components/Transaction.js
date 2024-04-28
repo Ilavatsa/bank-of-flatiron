2129341be6310b8859183c17f8263b9172743dff
Felicity <felicitymuhonja33@gmail.com>
2024-04-28 20:16:44 +0300

    add bank-of-flatiron

---
+++ bank-of-flatiron/src/components/Transaction.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Transaction({date,description,category,amount,id,onDeleteKey}) {
+  function deleteTransaction(event){
+    
+    onDeleteKey(event.target.id)
+  
+  }
+  return (
+    <tr>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+      <td><button id={id} onClick={deleteTransaction}>Delete</button></td>
+    </tr>
+  );
+}
+
+export default Transaction;
